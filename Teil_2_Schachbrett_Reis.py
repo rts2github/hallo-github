ef1d3f67e255cf4275df37e6711932512d791bc2
temp1user <1time4you@web.de>
2021-06-07 00:49:47 +0200

    Noch unklar

--- Teil_2_Schachbrett_Reis.py
+++ Teil_2_Schachbrett_Reis.py
@@ -8,5 +8,6 @@ for feld in range(64):
 
 gewicht = summe *0.02 /1000/1000
 print()
-print("Wenn ein Reiskorn 0,02g wiegr, wiegen die gesamten" \
+print("Änderung_01 Wenn ein Reiskorn 0,02g wiegr, wiegen die gesamten" \
       "Reiskörner {:,.0f} Tonnen".format(gewicht))
+
